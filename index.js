b87b51f840c8ec97c705be993b04798164621f10
Raehale <87107302+Raehale@users.noreply.github.com>
2024-05-27 09:55:10 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,71 @@
-// javascript
+/**change title to reflect current subject */
+const currentSubjectEl = document.getElementById("current-subject");
+const htmlSubjectBtn = document.getElementById("html-subject");
+const cssSubjectBtn = document.getElementById("css-subject");
+const jsSubjectBtn = document.getElementById("js-subject");
+const reactSubjectBtn = document.getElementById("react-subject");
+const miscSubjectBtn = document.getElementById("misc-subject");
 
-console.log("Hello world");
+const htmlSectionEl = document.getElementById("html-section");
+const cssSectionEl = document.getElementById("css-section");
+const jsSectionEl = document.getElementById("js-section");
+const reactSectionEl = document.getElementById("react-section");
+const miscSectionEl = document.getElementById("misc-section");
+
+currentSubjectEl.textContent = htmlSubjectBtn.textContent;
+htmlSectionEl.removeAttribute("hidden");
+
+htmlSubjectBtn.addEventListener("click", function(){
+    switchTitle(htmlSubjectBtn);
+    
+    htmlSectionEl.hidden = false;
+    
+    cssSectionEl.hidden = true;
+    jsSectionEl.hidden = true;
+    reactSectionEl.hidden = true;
+    miscSectionEl.hidden = true;
+});
+cssSubjectBtn.addEventListener("click", function(){
+    switchTitle(cssSubjectBtn);
+
+    cssSectionEl.hidden = false;
+
+    htmlSectionEl.hidden = true;
+    jsSectionEl.hidden = true;
+    reactSectionEl.hidden = true;
+    miscSectionEl.hidden = true;
+});
+jsSubjectBtn.addEventListener("click", function(){
+    switchTitle(jsSubjectBtn);
+
+    jsSectionEl.hidden = false;
+    
+    htmlSectionEl.hidden = true;
+    cssSectionEl.hidden = true;
+    reactSectionEl.hidden = true;
+    miscSectionEl.hidden = true;
+});
+reactSubjectBtn.addEventListener("click", function(){
+    switchTitle(reactSubjectBtn);
+
+    reactSectionEl.hidden = false;
+    
+    htmlSectionEl.hidden = true;
+    cssSectionEl.hidden = true;
+    jsSectionEl.hidden = true;
+    miscSectionEl.hidden = true;
+});
+miscSubjectBtn.addEventListener("click", function(){
+    switchTitle(miscSubjectBtn);
+
+    miscSectionEl.hidden = false;
+
+    htmlSectionEl.hidden = true;
+    cssSectionEl.hidden = true;
+    jsSectionEl.hidden = true;
+    reactSectionEl.hidden = true;
+});
+
+function switchTitle(text) {
+    currentSubjectEl.textContent = text.textContent;
+}
